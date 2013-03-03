2165534870cd2203c04c8ece9627cd6947ee1fa5
Makis Tracend <makis.tracend@gmail.com>
2013-03-03 06:00:10 -0800

    using access token if supplied

--- backbone.api.facebook.js
+++ backbone.api.facebook.js
@@ -8,20 +8,36 @@
 	APP.Facebook = {};
 	
 	Backbone.API.Facebook = Backbone.Collection.extend({
+		// available options
+		options : {
+			access_token : false
+		}, 
 		fetch : function(method, model, options) {
 			var self = this;
-			FB.getLoginStatus(function(response){
-				if( response.status == "connected" ){
-					// continue with request
-					Backbone.Collection.prototype.fetch.call( self );
-				}
-				// else try to FB.Login?
-			});
+			
+			if( this.options.access_token ){ 
+				// we'll be using the supplied access token
+				Backbone.Collection.prototype.fetch.call( self );
+			} else {
+				FB.getLoginStatus(function(response){
+					if( response.status == "connected" ){
+						// continue with request
+						Backbone.Collection.prototype.fetch.call( self );
+					}
+					// else try to FB.Login?
+				});
+			}
 		}, 
 		sync : function(method, model, options) {
 			
 			var url = (this.url instanceof Function) ? this.url() : this.url;
+			var params = {};
+			// add access token if available
+			if( this.options.access_token ){ 
+				params["access_token"] =  this.options.access_token;
+			}
 			
+			//FB.api(url, method, params, function( response ) {
 			FB.api(url, function( response ) {
 				// save response.paging for later?
 				// send just the response.data:
@@ -31,6 +47,7 @@
 			
 		},
 		parse : function( response ){
+			//console.log("facebook data:", response );
 			//is uid always the id? apparently...
 			var data = _.map(response, function( result ){
 				if( result.uid ){
@@ -77,13 +94,23 @@
 	});
 
 	APP.Facebook.Feed = Backbone.API.Facebook.extend({
+		// examples options:
+		options: {
+			//access_token : config.facebook.access_token
+		}, 
 		model : Backbone.API.Facebook.Models.Feed,
 		url: function(){
-			return {
+			// creating an object of parameters
+			var params = {
 				method: 'fql.query',
 				query: "select message,description,attachment,created_time from stream where source_id ='"+ this.id +"' limit "+ this.num,
-				access_token: config.facebook.access_token
 			}
+			// add access token if available
+			if( this.options.access_token ){ 
+				// we'll be using the supplied access token
+				params["access_token"] = this.options.access_token;
+			}
+			return params;
 			// old...
 			// check if there is either an id or user set - fallback to 'me'
 			//var page = this.user || this.id || "me";
